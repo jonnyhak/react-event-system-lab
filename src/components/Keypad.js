d5206a19814a1217396c5987e5364ed3877b5688
jonnyhak <jonnyhak@gmail.com>
2020-12-09 17:09:24 -0500

    commit tree d857a14d8dabbd0269c49c875b6366849d0bfe2f

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+
+// import { render } from 'enzyme'
+import React from 'react'
+
+class Keypad extends React.Component {
+
+    keyHandler = () => {
+        console.log("Entering password...")
+    }
+    
+    
+    render() {
+
+        return (
+            <div>
+                <input onKeyUp={this.keyHandler} type="password" />
+            </div>
+        )
+    }
+} 
+
+export default Keypad
